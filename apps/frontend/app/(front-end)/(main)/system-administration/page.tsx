8b78a276f9843cdf51789989da76fcb0a87cd903
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-11-15 15:46:41 +0700

    add coming soon

---
+++ apps/frontend/app/(front-end)/(main)/system-administration/page.tsx
@@ -0,0 +1,11 @@
+import ComingSoon from '@/components/ComingSoon'
+import React from 'react'
+
+
+const SystemAdministrationPage = () => {
+    return (
+        <ComingSoon title="System Administration" />
+    )
+}
+
+export default SystemAdministrationPage
