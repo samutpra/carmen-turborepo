b393cbce122a16aa6e57b24c9abb5bde512cbc86
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-11-15 14:29:42 +0700

    add store operation page

---
+++ apps/frontend/app/(front-end)/(main)/store-operations/stock-replenishment/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StockReplenishmentPage = () => {
+    return (
+        <div>StockReplenishmentPage</div>
+    )
+}
+
+export default StockReplenishmentPage
---
+++ apps/frontend/app/(front-end)/(main)/store-operations/store-requisitions/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StoreRequisitionsPage = () => {
+    return (
+        <div>StoreRequisitionsPage</div>
+    )
+}
+
+export default StoreRequisitionsPage
