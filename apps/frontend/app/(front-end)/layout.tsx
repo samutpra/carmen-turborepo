264b465fdddf8229c5130ee9d83a06afa8fad3a8
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-11-15 14:53:46 +0700

    Update layout.tsx

--- apps/frontend/app/(front-end)/layout.tsx
+++ apps/frontend/app/(front-end)/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
     <LanguageProvider>
       <html lang={languageTag()}>
         <body
-        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           <ThemeProvider
             attribute="class"
