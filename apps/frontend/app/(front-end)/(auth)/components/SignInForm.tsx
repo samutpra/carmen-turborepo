d8faaac879f994f6bd8055ff9e6c790ba30de4eb
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-11-17 16:02:10 +0700

    new layout

---
+++ apps/frontend/app/(front-end)/(auth)/components/SignInForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SignInForm = () => {
+    return (
+
+        <div className='flex flex-col justify-center items-center h-[100vh]'>
+            <div>SignInForm</div>
+        </div>
+    )
+}
+
+export default SignInForm
---
+++ apps/frontend/app/(front-end)/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignInForm from "../components/SignInForm"
+
+const SignInPage = () => {
+    return (
+        <SignInForm />
+    )
+}
+
+export default SignInPage
