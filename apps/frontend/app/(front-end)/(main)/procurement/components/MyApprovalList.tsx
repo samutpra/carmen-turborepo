c8d2416de335340f1e6c35ad29e15c77498496b1
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-11-28 16:32:02 +0700

    update lang

--- apps/frontend/app/(front-end)/(main)/procurement/components/MyApprovalList.tsx
+++ apps/frontend/app/(front-end)/(main)/procurement/components/MyApprovalList.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import NotiApproval from './NotiApproval'
 import RecentApprovals from './RecentApprovals'
 import PrqApproval from './PrqApproval'
-
+import * as m from '@/paraglide/messages.js';
 const MyApprovalList = () => {
     return (
         <div className='container mx-auto p-6'>
-            <h1 className="text-3xl font-semibold mb-6">Department Approval</h1>
+            <h1 className="text-3xl font-semibold mb-6">{m.department_approve()}</h1>
             <div className="grid gap-6 lg:grid-cols-3">
                 <PrqApproval />
                 <div className='space-y-6'>
