e3005b53a508d9e9e788909fd845f64547480ae8
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-12-02 13:31:19 +0700

    Update types.ts

--- apps/frontend/lib/types.ts
+++ apps/frontend/lib/types.ts
@@ -1442,23 +1442,6 @@ export interface CustomJwtPayload extends JwtPayload {
   username?: string
 }
 
-// export const LocationSchema = z.object({
-//   id: z.string().uuid().optional(),
-//   name: z.string(),
-//   location_type: z.enum(['inventory', 'direct']),
-//   description: z.string(),
-//   is_active: z.boolean(),
-//   delivery_point_id: z.string().optional().nullable(),
-// });
-
-// export type LocationType = z.infer<typeof LocationSchema>;
-
-// export type PayloadLocationType = Omit<LocationType, 'id'>;
-
-// export interface LocationLabel {
-//   key: keyof LocationType;
-//   label: string;
-// }
 
 export const DepartmentSchema = z.object({
   id: z.string().optional(),
