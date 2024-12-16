c209177aae9aac1870727c0746be10e135518ea2
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-12-16 16:22:06 +0700

    Update MenuDialog.tsx

--- apps/frontend/components/Navbar/MenuDialog.tsx
+++ apps/frontend/components/Navbar/MenuDialog.tsx
@@ -46,12 +46,12 @@ const MenuDialog = () => {
 					<LayoutPanelLeft />
 				</Button>
 			</DialogTrigger>
-			<DialogContent className="max-h-[800px] overflow-y-auto w-full md:w-2/3 p-4">
+			<DialogContent className="max-h-[800px] overflow-y-auto w-full md:w-2/3 px-4 py-12">
 				<div className="grid grid-cols-3 md:grid-cols-6 gap-6 w-full">
 					{menuItems.map((item, index) => {
 						const IconComponent = iconMap[item.icon];
 						return (
-							<div key={index} className="relative">
+							<div key={index} className="relative ">
 								<Link href={item.path}>
 									<Card className="aspect-square flex flex-col items-center justify-center hover:bg-slate-50 relative">
 										<div className="absolute top-0 right-1 flex gap-1">
