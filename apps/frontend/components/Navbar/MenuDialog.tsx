8e6457cbd84924d1262c534f680bf619c078331e
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-12-16 13:44:47 +0700

    Update MenuDialog.tsx

--- apps/frontend/components/Navbar/MenuDialog.tsx
+++ apps/frontend/components/Navbar/MenuDialog.tsx
@@ -19,6 +19,7 @@ import {
 	DollarSign,
 	Settings,
 	HelpCircle,
+	LockKeyhole,
 } from 'lucide-react';
 import { menuItems } from '@/lib/util/menuItems';
 import { Link } from '@/lib/i18n';
@@ -53,16 +54,22 @@ const MenuDialog = () => {
 					{menuItems.map((item, index) => {
 						const IconComponent = iconMap[item.icon];
 						return (
-							<Link key={index} href={item.path}>
-								<Card className="aspect-square flex flex-col items-center justify-center hover:bg-slate-50">
-									<CardContent className="flex flex-col items-center justify-center gap-2 p-4">
-										{IconComponent && <IconComponent className="w-12 h-12" />}
-									</CardContent>
-								</Card>
-								<p className="mt-2 font-medium text-xs text-center">
-									{item.title}
-								</p>
-							</Link>
+							<div key={index} className="relative">
+								<Link href={item.path}>
+									<Card className="aspect-square flex flex-col items-center justify-center hover:bg-slate-50 relative">
+										<div className="absolute top-[-10px] right-0 flex gap-1">
+											<LockKeyhole className="w-4 h-4 text-gray-500 hover:text-gray-700" />
+											<ShoppingCart className="w-4 h-4 text-gray-500 hover:text-gray-700" />
+										</div>
+										<CardContent className="flex flex-col items-center justify-center gap-2 p-4">
+											{IconComponent && <IconComponent className="w-10 h-10" />}
+										</CardContent>
+									</Card>
+									<p className="mt-2 font-medium text-xs text-center">
+										{item.title}
+									</p>
+								</Link>
+							</div>
 						);
 					})}
 				</div>
