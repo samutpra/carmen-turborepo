9bf400dac11f1fd9c0c7494a0e88d0914fa22cac
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-12-17 19:26:08 +0700

    crud deliapi

---
+++ apps/frontend/app/(front-end)/api/configuration/delivery-point/route.ts
@@ -0,0 +1,82 @@
+import { API_URL } from '@/lib/util/api';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const GET = async (request: NextRequest) => {
+	try {
+		const token = request.headers.get('Authorization')?.replace('Bearer ', '');
+		if (!token) {
+			return NextResponse.json(
+				{ error: 'Token or tenant ID is missing from the headers' },
+				{ status: 400 }
+			);
+		}
+		const options = {
+			method: 'GET',
+			headers: {
+				Authorization: `Bearer ${token}`,
+				'x-tenant-id': 'DUMMY',
+				'Content-Type': 'application/json',
+			},
+		};
+		const response = await fetch(`${API_URL}/v1/delivery-point`, options);
+		if (response.status === 401) {
+			return NextResponse.json(
+				{ error: 'Unauthorized access - Invalid or expired token' },
+				{ status: 401 }
+			);
+		}
+		if (!response.ok) {
+			throw new Error(
+				`Failed to fetch units: ${response.status} ${response.statusText}`
+			);
+		}
+		const data = await response.json();
+		return NextResponse.json({
+			data,
+		});
+	} catch (error) {
+		return NextResponse.json(
+			{ message: 'Internal Server Error', error: error },
+			{ status: 500 }
+		);
+	}
+};
+
+export const POST = async (request: NextRequest) => {
+	try {
+		const token = request.headers.get('Authorization')?.replace('Bearer ', '');
+		if (!token) {
+			return NextResponse.json(
+				{ error: 'Token is missing from the headers' },
+				{ status: 401 }
+			);
+		}
+		const data = await request.json();
+		const options = {
+			method: 'POST',
+			headers: {
+				Authorization: `Bearer ${token}`,
+				'x-tenant-id': 'DUMMY',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				name: data.name,
+				is_active: data.is_active ?? true,
+			}),
+		};
+		const response = await fetch(`${API_URL}/v1/delivery-point`, options);
+
+		if (!response.ok) {
+			throw new Error(
+				`Failed to create delivery point: ${response.status} ${response.statusText}`
+			);
+		}
+		const result = await response.json();
+		return NextResponse.json(result, { status: 200 });
+	} catch (error) {
+		return NextResponse.json(
+			{ message: 'Internal Server Error', error: error },
+			{ status: 500 }
+		);
+	}
+};
