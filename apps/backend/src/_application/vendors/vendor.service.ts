2601de583f87deb93a4e84718cebf57fb00a98e3
Thammanoon Semapru <samutpra@hotmail.com>
2024-12-18 09:35:45 +0700

    Refactor Vendor API: Updated VendorController and VendorService to implement CRUD operations with improved logging and error handling. Changed API route structure to include versioning and enhanced query parameter handling. Updated Prettier configuration to enforce single quotes.

--- apps/backend/src/_application/vendors/vendor.service.ts
+++ apps/backend/src/_application/vendors/vendor.service.ts
@@ -1,4 +1,165 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ResponseId,
+  ResponseList,
+  ResponseSingle,
+} from 'lib/helper/iResponse';
+import QueryParams from 'lib/types';
+import { DuplicateException } from 'lib/utils';
+import {
+  ExtractReqService,
+} from 'src/_lib/auth/extract-req/extract-req.service';
+import {
+  PrismaClientManagerService,
+} from 'src/_lib/prisma-client-manager/prisma-client-manager.service';
+
+import {
+  VendorCreateDto,
+  VendorUpdateDto,
+} from '@carmensoftware/shared-dtos';
+import {
+  HttpStatus,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+import {
+  PrismaClient as dbTenant,
+  tb_vendor,
+} from '@prisma-carmen-client-tenant';
 
 @Injectable()
-export class VendorService {}
+export class VendorService {
+  private db_tenant: dbTenant;
+
+  constructor(
+    private prismaClientMamager: PrismaClientManagerService,
+    private extractReqService: ExtractReqService,
+  ) {}
+
+  logger = new Logger(VendorService.name);
+
+  async _getById(db_tenant: dbTenant, id: string): Promise<tb_vendor> {
+    const res = await db_tenant.tb_vendor.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    return res;
+  }
+
+  async findOne(req: Request, id: string): Promise<ResponseSingle<tb_vendor>> {
+    const { user_id, business_unit_id } = this.extractReqService.getByReq(req);
+    this.db_tenant = this.prismaClientMamager.getTenantDB(business_unit_id);
+    const oneObj = await this._getById(this.db_tenant, id);
+
+    if (!oneObj) {
+      throw new NotFoundException('Vendor not found');
+    }
+    const res: ResponseSingle<tb_vendor> = {
+      data: oneObj,
+    };
+    return res;
+  }
+
+  async findAll(
+    req: Request,
+    q: QueryParams,
+  ): Promise<ResponseList<tb_vendor>> {
+    const { user_id, business_unit_id } = this.extractReqService.getByReq(req);
+    this.db_tenant = this.prismaClientMamager.getTenantDB(business_unit_id);
+    const max = await this.db_tenant.tb_vendor.count({
+      where: q.where(),
+    });
+    const listObj = await this.db_tenant.tb_vendor.findMany(q.findMany());
+
+    const res: ResponseList<tb_vendor> = {
+      data: listObj,
+      pagination: {
+        total: max,
+        page: q.page,
+        perPage: q.perPage,
+        pages: max == 0 ? 1 : Math.ceil(max / q.perPage),
+      },
+    };
+    return res;
+  }
+
+  async create(
+    req: Request,
+    createDto: VendorCreateDto,
+  ): Promise<ResponseId<string>> {
+    const { user_id, business_unit_id } = this.extractReqService.getByReq(req);
+    this.db_tenant = this.prismaClientMamager.getTenantDB(business_unit_id);
+
+    const found = await this.db_tenant.tb_vendor.findUnique({
+      where: {
+        name: createDto.name,
+      },
+    });
+
+    if (found) {
+      throw new DuplicateException({
+        statusCode: HttpStatus.CONFLICT,
+        message: 'Vendor already exists',
+        id: found.id,
+      });
+    }
+
+    const createObj = await this.db_tenant.tb_vendor.create({
+      data: {
+        ...createDto,
+        created_by_id: user_id,
+        created_at: new Date(),
+        updated_by_id: user_id,
+        updated_at: new Date(),
+      },
+    });
+
+    const res: ResponseId<string> = { id: createObj.id };
+
+    return res;
+  }
+
+  async update(
+    req: Request,
+    id: string,
+    updateDto: VendorUpdateDto,
+  ): Promise<ResponseId<string>> {
+    const { user_id, business_unit_id } = this.extractReqService.getByReq(req);
+    this.db_tenant = this.prismaClientMamager.getTenantDB(business_unit_id);
+    const oneObj = await this._getById(this.db_tenant, id);
+
+    if (!oneObj) {
+      throw new NotFoundException('Vendor not found');
+    }
+
+    const updateObj = await this.db_tenant.tb_vendor.update({
+      where: {
+        id,
+      },
+      data: { ...updateDto, updated_by_id: user_id, updated_at: new Date() },
+    });
+
+    const res: ResponseId<string> = {
+      id: updateObj.id,
+    };
+
+    return res;
+  }
+
+  async delete(req: Request, id: string) {
+    const { user_id, business_unit_id } = this.extractReqService.getByReq(req);
+    this.db_tenant = this.prismaClientMamager.getTenantDB(business_unit_id);
+    const oneObj = await this._getById(this.db_tenant, id);
+
+    if (!oneObj) {
+      throw new NotFoundException('Vendor not found');
+    }
+
+    await this.db_tenant.tb_vendor.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
