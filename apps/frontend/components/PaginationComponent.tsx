d20668ccc4434e42e9cf245eb05ae8d8cb9ea4c4
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2025-01-27 15:16:34 +0700

    Update PaginationComponent.tsx

--- apps/frontend/components/PaginationComponent.tsx
+++ apps/frontend/components/PaginationComponent.tsx
@@ -31,113 +31,131 @@ const PaginationComponent = ({
     if (totalPages <= 1) return null;
 
     return (
-        <Pagination className="flex justify-end mt-4 items-center">
-            <PaginationContent>
-                <PaginationItem>
-                    <ChevronLeft
-                        href="#"
-                        onClick={(e) => {
-                            e.preventDefault();
-                            handlePageChange(currentPage - 1);
-                        }}
-                        size={18}
-                        className={currentPage <= 1 ? 'pointer-events-none opacity-50 mr-2' : 'cursor-pointer'}
-                    />
-                </PaginationItem>
+			<Pagination
+				className="flex justify-end mt-4 items-center"
+				data-id="pagination-container"
+			>
+				<PaginationContent data-id="pagination-content">
+					<PaginationItem data-id="pagination-item">
+						<ChevronLeft
+							href="#"
+							onClick={(e) => {
+								e.preventDefault();
+								handlePageChange(currentPage - 1);
+							}}
+							size={18}
+							className={
+								currentPage <= 1
+									? 'pointer-events-none opacity-50 mr-2'
+									: 'cursor-pointer'
+							}
+							data-id="pagination-chevron-left"
+						/>
+					</PaginationItem>
 
-                <PaginationItem>
-                    <PaginationLink
-                        href="#"
-                        isActive={currentPage === 1}
-                        onClick={(e) => {
-                            e.preventDefault();
-                            handlePageChange(1);
-                        }}
-                    >
-                        1
-                    </PaginationLink>
-                </PaginationItem>
+					<PaginationItem data-id="pagination-item">
+						<PaginationLink
+							href="#"
+							isActive={currentPage === 1}
+							onClick={(e) => {
+								e.preventDefault();
+								handlePageChange(1);
+							}}
+							data-id="pagination-link"
+						>
+							1
+						</PaginationLink>
+					</PaginationItem>
 
-                {currentPage > 3 && (
-                    <PaginationItem>
-                        <PaginationEllipsis />
-                    </PaginationItem>
-                )}
+					{currentPage > 3 && (
+						<PaginationItem data-id="pagination-item">
+							<PaginationEllipsis data-id="pagination-ellipsis" />
+						</PaginationItem>
+					)}
 
-                {currentPage > 2 && (
-                    <PaginationItem>
-                        <PaginationLink
-                            href="#"
-                            onClick={(e) => {
-                                e.preventDefault();
-                                handlePageChange(currentPage - 1);
-                            }}
-                        >
-                            {currentPage - 1}
-                        </PaginationLink>
-                    </PaginationItem>
-                )}
+					{currentPage > 2 && (
+						<PaginationItem data-id="pagination-item">
+							<PaginationLink
+								href="#"
+								onClick={(e) => {
+									e.preventDefault();
+									handlePageChange(currentPage - 1);
+								}}
+								data-id="pagination-link"
+							>
+								{currentPage - 1}
+							</PaginationLink>
+						</PaginationItem>
+					)}
 
-                {currentPage !== 1 && currentPage !== totalPages && (
-                    <PaginationItem>
-                        <PaginationLink
-                            href="#"
-                            isActive={true}
-                        >
-                            {currentPage}
-                        </PaginationLink>
-                    </PaginationItem>
-                )}
+					{currentPage !== 1 && currentPage !== totalPages && (
+						<PaginationItem data-id="pagination-item">
+							<PaginationLink
+								href="#"
+								isActive={true}
+								data-id="pagination-link"
+							>
+								{currentPage}
+							</PaginationLink>
+						</PaginationItem>
+					)}
 
-                {currentPage < totalPages - 1 && (
-                    <PaginationItem>
-                        <PaginationLink
-                            href="#"
-                            onClick={(e) => {
-                                e.preventDefault();
-                                handlePageChange(currentPage + 1);
-                            }}
-                        >
-                            {currentPage + 1}
-                        </PaginationLink>
-                    </PaginationItem>
-                )}
+					{currentPage < totalPages - 1 && (
+						<PaginationItem data-id="pagination-item">
+							<PaginationLink
+								href="#"
+								onClick={(e) => {
+									e.preventDefault();
+									handlePageChange(currentPage + 1);
+								}}
+								data-id="pagination-link"
+							>
+								{currentPage + 1}
+							</PaginationLink>
+						</PaginationItem>
+					)}
 
-                {currentPage < totalPages - 2 && (
-                    <PaginationItem>
-                        <PaginationEllipsis />
-                    </PaginationItem>
-                )}
+					{currentPage < totalPages - 2 && (
+						<PaginationItem data-id="pagination-item">
+							<PaginationEllipsis data-id="pagination-ellipsis" />
+						</PaginationItem>
+					)}
 
-                {totalPages > 1 && (
-                    <PaginationItem>
-                        <PaginationLink
-                            href="#"
-                            isActive={currentPage === totalPages}
-                            onClick={(e) => {
-                                e.preventDefault();
-                                handlePageChange(totalPages);
-                            }}
-                        >
-                            {totalPages}
-                        </PaginationLink>
-                    </PaginationItem>
-                )}
+					{totalPages > 1 && (
+						<PaginationItem data-id="pagination-item">
+							<PaginationLink
+								href="#"
+								isActive={currentPage === totalPages}
+								onClick={(e) => {
+									e.preventDefault();
+									handlePageChange(totalPages);
+								}}
+								data-id="pagination-link"
+							>
+								{totalPages}
+							</PaginationLink>
+						</PaginationItem>
+					)}
 
-                <PaginationItem>
-                    <ChevronRight
-                        href="#"
-                        onClick={(e) => {
-                            e.preventDefault();
-                            handlePageChange(currentPage + 1);
-                        }}
-                        className={currentPage >= totalPages ? 'pointer-events-none opacity-50 mr-2' : 'cursor-pointer'}
-                        size={18}
-                    />
-                </PaginationItem>
-            </PaginationContent>
-        </Pagination>
-    );
+					<PaginationItem data-id="pagination-item">
+						<ChevronRight
+							href="#"
+							onClick={(e) => {
+								e.preventDefault();
+								handlePageChange(currentPage + 1);
+							}}
+							className={
+								currentPage >= totalPages
+									? 'pointer-events-none opacity-50 mr-2'
+									: 'cursor-pointer'
+							}
+							size={18}
+							data-id="pagination-chevron-right"
+						/>
+					</PaginationItem>
+				</PaginationContent>
+			</Pagination>
+		);
 };
 
 export default PaginationComponent;
