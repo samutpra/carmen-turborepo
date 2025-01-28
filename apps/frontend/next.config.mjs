51a5301d4fa434b2a9d0c787518c39821c509b77
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2025-01-28 13:18:04 +0700

    dockerfile

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -1,19 +1,24 @@
 /** @type {import('next').NextConfig} */
 
-import { paraglide } from "@inlang/paraglide-next/plugin";
+import { paraglide } from '@inlang/paraglide-next/plugin';
 
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  transpilePackages: ["lucide-react"],
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	logging: {
+		fetches: {
+			fullUrl: true,
+			hmrRefreshes: true,
+		},
+	},
+	transpilePackages: ['lucide-react'],
 };
 
 export default paraglide({
-  paraglide: {
-    project: "./project.inlang",
-    outdir: "./paraglide",
-  },
-  ...nextConfig,
+	paraglide: {
+		project: './project.inlang',
+		outdir: './paraglide',
+	},
+	...nextConfig,
 });
