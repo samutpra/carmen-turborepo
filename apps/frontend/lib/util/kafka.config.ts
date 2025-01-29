2b54a7ac66b85ea2e442e012c9059483c7b5f314
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2025-01-29 10:08:46 +0700

    Update kafka.config.ts

--- apps/frontend/lib/util/kafka.config.ts
+++ apps/frontend/lib/util/kafka.config.ts
@@ -1,8 +1,6 @@
 const KAFKA_CONFIG = {
-	// host: process.env.KAFKA_HOST || 'localhost:9092',
-	// topic: process.env.KAFKA_TOPIC || 'my-topic',
-	host: 'localhost:9092',
-	topic: 'my-topic',
+	host: process.env.NEXT_PUBLIC_KAFKA_HOST || 'localhost:9092',
+	topic: process.env.NEXT_PUBLIC_KAFKA_TOPIC || 'my-topic',
 	timeout: 30000, // 30 seconds timeout
 	partition: 0,
 } as const;
