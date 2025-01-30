d09597a7bf7233e0adf49245bfe24c3673f05a9f
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2025-01-30 14:00:27 +0700

    delete page

--- apps/frontend/app/(front-end)/(main)/operational-planning/recipe-management/costing/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CostingPage = () => {
-    return (
-        <div>CostingPage</div>
-    )
-}
-
-export default CostingPage
--- apps/frontend/app/(front-end)/(main)/operational-planning/recipe-management/media/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Media_OP_Page = () => {
-    return (
-        <div>Media_OP_Page</div>
-    )
-}
-
-export default Media_OP_Page
